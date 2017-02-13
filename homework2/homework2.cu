96a379c38581912411ed7bfb7426f467aa42b2e7
Wenchang Zhang <npsfsz@gmail.com>
2017-02-13 14:50:43 -0800

    msg

--- homework2/homework2.cu
+++ homework2/homework2.cu
@@ -427,7 +427,7 @@ int main(){
 	// copy final sum from device to host
 	int gpu_result;
     cudaMemcpy(&gpu_result, d_oarray, sizeof(int), cudaMemcpyDeviceToHost);
-    
+    printf("final result is %d", gpu_result);
     return 0;
 }
 
