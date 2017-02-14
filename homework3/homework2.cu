17f3cb1e3526bcd0aa0d614702e7a68d1771cac9
npsfsz <npsfsz@gmail.com>
2017-02-14 18:41:02 -0500

    final version

--- homework3/homework2.cu
+++ homework3/homework2.cu
@@ -33,21 +33,22 @@ unsigned int nextPow2(unsigned int x)
 double CPUtime(){
        struct timeval tp;
        gettimeofday (&tp, NULL);
-       return ((double)tp.tv_sec + (double)tp.tv_usec * 1e-6);
+       return ((double)tp.tv_sec *1e6 + (double)tp.tv_usec );
 }
 
 
 
 __global__ void
 reduce(int *d_iarray, int *d_oarray, int n, int blockSize){
-    __shared__ int sdata[256]; //hard coded for now
-
+    //__shared__ int sdata[32]; //hard coded for now hard limit 0xc000
+	__device__ __shared__ int sdata[64];
     // perform first level of reduction,
     // reading from global memory, writing to shared memory
     int tid = threadIdx.x;
     int i = blockIdx.x*blockSize*2 + threadIdx.x;
     int gridSize = blockSize*2*gridDim.x;
 
+    //second round n = 64, blockSize = 32
     int mySum = 0;
     
     // we reduce multiple elements per thread.  The number is determined by the
@@ -352,97 +353,148 @@ void getNumBlocksAndThreads(int whichKernel, int n, int maxBlocks, int maxThread
 
 
 int main(){
-    printf("Starting program... preparing array\n");
+	int iteration = 20;
+	double firstR, secondR;
+    printf("Starting program... \n");
+    //printf("preparing array\n");
 	int *h_array = generateRandomArray(size); //512M size
-	printf("array generate complete\n");
+	//printf("array generate complete\n");
 	int *d_iarray, *d_oarray;
-
-	int bytes = sizeof(int) * size;
-	//printf("%b\n", bytes);
-	int maxThreads = 256; //number of threads per block
-	int maxBlocks = 64;
 	int blocks = 0; //the following two should be maximum
 	int threads = 0;
+	int maxThreads = 4096; //number of threads per block
+	int maxBlocks = 1024;
+	firstR = 0;
+	secondR = 0;
+	for (blocks = 32; blocks <= 32/*maxBlocks*/; blocks *= 2){
+		for (threads = 512 ;threads <= 512/*maxThreads*/; threads *= 2){
+			for (int i = 0; i < iteration; i++){
+				//int bytes = sizeof(int) * size;
+				//printf("%b\n", bytes);
 
 
-  	//d_iarray = (int*)malloc(bytes);
-	//d_oarray = (int*)malloc(maxBlocks*sizeof(int));
-	//alloc mem on GPU
-	//int *d_array;
-	printf("copy data to GPU\n");
-
-	cudaMalloc((void **)&d_iarray, size * sizeof(int));
-	//printf("1\n");
-	cudaMalloc((void **)&d_oarray, maxBlocks * sizeof(int));
-
-	//copy data to GPU
-	cudaMemcpy(d_iarray, h_array, size * sizeof(int), cudaMemcpyHostToDevice);
-    printf("copy complete\n");
-	//do the work
-	
-	getNumBlocksAndThreads(6, size, maxBlocks, maxThreads, blocks, threads);
-	//define struct
-	dim3 block(threads, 1, 1);
-	dim3 grid(blocks, 1, 1);
-	// when there is only one warp per block, we need to allocate two warps
-    // worth of shared memory so that we don't index shared memory out of bounds
-    int smemSize = (threads <= 32) ? 2 * threads * sizeof(int) : threads * sizeof(int);
-	
-	printf("first round of reduction\n");
-	//first round of reduction
-	reduce<<< grid, block, smemSize >>>(d_iarray, d_oarray, size, 256);
-	printf("complete first round\n");
-	// Clear d_idata for later use as temporary buffer.
-    cudaMemset(d_iarray, 0, size*sizeof(int));
-    
-    // sum partial block sums on GPU
-    int s=blocks;
 
 
-    while (s > 1)
-    {
-        int threads = 0, blocks = 0;
-        getNumBlocksAndThreads(6, s, maxBlocks, maxThreads, blocks, threads);//1 block 32 threads
-        cudaMemcpy(d_iarray, d_oarray, s*sizeof(int), cudaMemcpyDeviceToDevice);//prepare new input date
-        //reduce<T>(s, threads, blocks, kernel, d_idata, d_odata);//reduce
-        
-        int smemSize = (threads <= 32) ? 2 * threads * sizeof(int) : threads * sizeof(int);
-        printf("second round of reduction\n");
-        reduce<<< grid, block, smemSize >>>(d_iarray, d_oarray, s, 32);
-        //1 block 32 threads, 
-        printf("complete second round\n");
+
+				//d_iarray = (int*)malloc(bytes);
+				//d_oarray = (int*)malloc(maxBlocks*sizeof(int));
+				//alloc mem on GPU
+				//int *d_array;
+				//printf("copy data to GPU\n");
+
+				cudaMalloc((void **)&d_iarray, size * sizeof(int));
+				//printf("1\n");
+				cudaMalloc((void **)&d_oarray, maxBlocks * sizeof(int));
+
+				//copy data to GPU
+				cudaMemcpy(d_iarray, h_array, size * sizeof(int), cudaMemcpyHostToDevice);
+				//printf("copy complete\n");
+				//do the work
+
+				//getNumBlocksAndThreads(6, size, maxBlocks, maxThreads, blocks, threads);
+				//define struct
 
 
-        s = (s + (threads*2-1)) / (threads*2);
 
-        /*
-        if (s > 1)
-        {
-            // copy result from device to host
-            cudaMemcpy(h_odata, d_odata, s * sizeof(T), cudaMemcpyDeviceToHost);
+				//blocks = 64;
+				//threads = 256;
+				dim3 grid(blocks, 1, 1);
+				dim3 block(threads, 1, 1);
 
-            for (int i=0; i < s; i++)
-            {
-                gpu_result += h_odata[i];
-            }
+				//dim3 grid(64,1,1);
+				//dim3 block(256,1,1);
+				// when there is only one warp per block, we need to allocate two warps
+				// worth of shared memory so that we don't index shared memory out of bounds
+				int smemSize = (threads <= 32) ? 2 * threads * sizeof(int) : threads * sizeof(int);
 
-            needReadBack = false;
-        }
-        */
+				//printf("first round of reduction\n");
+				//first round of reduction
+				cudaDeviceSynchronize();
+				double firstStart = CPUtime();
+
+				reduce<<< grid, block, smemSize >>>(d_iarray, d_oarray, size, threads);
+				cudaDeviceSynchronize();
+				double firstEnd = CPUtime();
+				//printf("complete first round, took time %f\n", firstEnd-firstStart);
+				firstR += firstEnd-firstStart;
+				// Clear d_idata for later use as temporary buffer.
+				cudaMemset(d_iarray, 0, size*sizeof(int));
+
+				// sum partial block sums on GPU
+				int s=blocks;
+
+				//secondR = 0;
+				while (s > 1)
+				{
+					int threads2 = 0, blocks2 = 0;
+					getNumBlocksAndThreads(6, s, maxBlocks, maxThreads, blocks2, threads2);//1 block 32 threads
+					cudaMemcpy(d_iarray, d_oarray, s*sizeof(int), cudaMemcpyDeviceToDevice);//prepare new input date
+					//reduce<T>(s, threads, blocks, kernel, d_idata, d_odata);//reduce
+
+
+					dim3 block2(threads2, 1, 1);
+					dim3 grid2(blocks, 1, 1);
+					//dim3 grid2(1, 1, 1);
+					//dim3 block2(32, 1, 1);
+
+					int smemSize = (threads2 <= 32) ? 2 * threads2 * sizeof(int) : threads2 * sizeof(int);
+					cudaDeviceSynchronize();
+					//printf("second round of reduction\n");
+					double secondStart = CPUtime();
+					reduce<<< grid2, block2, smemSize >>>(d_iarray, d_oarray, s, threads2);
+					cudaDeviceSynchronize();
+					//1 block 32 threads,
+					double secondEnd = CPUtime();
+					//printf("complete second round, took time %f\n", secondEnd - secondStart);
+					secondR += secondEnd-secondStart;
+
+					s = (s + (threads2*2-1)) / (threads2*2);
+
+					/*
+					if (s > 1)
+					{
+						// copy result from device to host
+						cudaMemcpy(h_odata, d_odata, s * sizeof(T), cudaMemcpyDeviceToHost);
+
+						for (int i=0; i < s; i++)
+						{
+							gpu_result += h_odata[i];
+						}
+
+						needReadBack = false;
+					}
+					*/
+				}
+
+				cudaFree(d_iarray);
+				cudaFree(d_oarray);
+
+			}
+			// copy final sum from device to host
+			double firstFinal = firstR/iteration;
+			double secondFinal = secondR/iteration;
+			//printf("average time, first round %f, second round %f, total %f\n", firstFinal, secondFinal, firstFinal+secondFinal);
+			int gpu_result;
+			cudaMemcpy(&gpu_result, d_oarray, sizeof(int), cudaMemcpyDeviceToHost);
+			//printf("final GPU result is %d\n", gpu_result);
+			int cpu_result = 0;
+			for (int i = 0; i < size; i++){
+				cpu_result += h_array[i];
+			}
+			//printf("final CPU result is %d\n", cpu_result);
+			printf("grid %d, block %d, average time, first round %.2f, second round %.2f, total %.2f",blocks, threads, firstFinal, secondFinal, firstFinal+secondFinal);
+			if (cpu_result == gpu_result){
+				printf(" result matches\n");
+			}else{
+				printf(" result not match\n");
+			}
+			cudaFree(d_iarray);
+			cudaFree(d_oarray);
+			cudaDeviceSynchronize();
+		}
 	}
-	cudaDeviceSynchronize();
-	
-	
-	// copy final sum from device to host
-	int gpu_result;
-    cudaMemcpy(&gpu_result, d_oarray, sizeof(int), cudaMemcpyDeviceToHost);
-    printf("final GPU result is %d\n", gpu_result);
-    int cpu_result;
-    for (int i = 0; i < size; i++){
-    	cpu_result += h_array[i];
-    }
-    printf("final CPU result is %d\n", cpu_result);
-    return 0;
+
+	return 0;
 }
 
 
